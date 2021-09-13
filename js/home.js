8eb492d8221b23859dc478ac9d44a1955456fe5c
Camilo Patino <cam.patino@gmail.com>
2021-09-12 23:08:33 -0500

    Contained navegation
    - Added iframe for main content
    - Added logic to render dynamic views
    - Created profile, ranking, pauses, home content views

--- js/home.js
+++ js/home.js
@@ -1,3 +1,22 @@
+let iframe;
+
 $(document).ready(function() {
-  
+  iframe = $("#iframe");
+
+  $("#homeLink").click(function() {
+    iframe.attr("src", "pages/home-content.html");
+  });
+
+  $("#profileLink").click(function() {
+    iframe.attr("src", "pages/profile.html");
+  });
+
+  $("#pausesLink").click(function() {
+    iframe.attr("src", "pages/pauses.html");
+  });
+
+  $("#rankingLink").click(function() {
+    iframe.attr("src", "pages/ranking.html");
+  });
+
 });
